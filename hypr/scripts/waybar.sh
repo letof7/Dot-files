09d66ed5242714eb53cba6dab8c53ed5fcd6096a
letof7 <31816885+letof7@users.noreply.github.com>
2024-05-12 21:00:17 +0200

    Delete hypr directory

--- hypr/scripts/waybar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-waybar &
-sleep 2
-killall waybar
-sleep 1
-#waybar --config /home/tof/.config/waybar/config & 
-waybar --config /home/tof/.config/waybar/config --style /home/tof/.config/waybar/style.css 
